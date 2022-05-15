1774324503d900f6cf5c69613a1388debb5f0d2c
AUDRIUS BEJERIS <audriusbejeris@gmail.com>
2022-05-15 13:28:03 +0100

    init

---
+++ app/script/script.js
@@ -0,0 +1,25 @@
+"use strict"
+var tabs = document.getElementsByClassName("tab");
+var buttons = document.getElementsByTagName("button")
+
+
+for (var x of tabs) {
+    x.style.display = "none";
+}
+
+function changeHeader(event, id) {
+    for (var x of tabs) {
+        x.style.display = "none";
+        x.classList.remove("tabAnimation")
+    }
+    document.getElementById(id).style.display = "block";
+    setTimeout(function () {
+        document.getElementById(id).classList.add("tabAnimation")
+    }, 200)
+
+    for (var x of buttons) {
+        x.classList.remove("selected")
+    }
+
+    event.currentTarget.classList.add("selected")
+}
